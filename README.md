de613e2f5fa3487519488a2b879b3f8944679cf7
Lewis Pollard <lewispollard@lewis-pc.(none)>
2012-08-23 14:11:50 +0100

    GPL Licensed

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ TunlrSwitcher
 A quick menubar application for Mac that lets you quickly switch between the [Tunlr DNS servers](http://www.tunlr.net) and default empty DNS settings on all adaptors.
 
 If the Tunlr servers change I will try and update this as quickly as possible. You can manually change the servers by clicking the TunlrSwitcher icon and choosing 'Preferences'.
+
+`Copyright (c) 2012 Lewis Pollard. All rights reserved
+This file is part of TunlrSwitcher.
+
+TunlrSwitcher is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TunlrSwitcher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.`
--- Resources/switch.sh
+++ Resources/switch.sh
@@ -5,6 +5,20 @@
 #
 #  Created by Lewis Pollard on 22/08/2012.
 #  Copyright (c) 2012 Lewis Pollard. All rights reserved
+#  This file is part of TunlrSwitcher.
+#
+#  TunlrSwitcher is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  TunlrSwitcher is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 # Takes 3 parameters, DNS1, DNS2 and whether this is the first run of the script for this session
 IFS=$'\n'
 adapters=`networksetup -listallnetworkservices |grep -v denotes`
